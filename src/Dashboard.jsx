85b53305012c2e0a106c0cf5bb41c691cd543366
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-25 08:24:44 +0100

    updates

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 export const Dashboard = () => {
-  const salesData = [];
+  const salesData = [
+    { month: "jan", sales: 7 },
+    { month: "feb", sales: 18 },
+    { month: "mar", sales: 4 },
+    { month: "apr", sales: 25 },
+    { month: "may", sales: 16 },
+    { month: "jun", sales: 30 },
+    { month: "jul", sales: 20 },
+    { month: "aug", sales: 45 },
+    { month: "sep", sales: 35 },
+    { month: "oct", sales: 9 },
+    { month: "nov", sales: 12 },
+    { month: "dec", sales: 26 },
+  ];
   return (
     <div className="flex bg-[#fafafa] w-screen h-screen">
       <div className="p-4 border rounded-2xl bg-[#fff] m-10 flex flex-col gap-6 h-max w-[60vw]">
@@ -12,68 +25,53 @@ export const Dashboard = () => {
             <div className="p-1 px-2 rounded-full border">weekly</div>
           </div>
         </div>
-        <div className="flex gap-4">
-          <div className="flex flex-col pb-6 justify-between">
-            <p>5.00</p>
-            <p>5.00</p>
-            <p>5.00</p>
-            <p>5.00</p>
-            <p>5.00</p>
-            <p>5.00</p>
-            <p>5.00</p>
+        <div className="flex gap-8 relative">
+          <div className="flex flex-col pb-5 justify-between">
+            <div className="flex items-center gap-4">
+              <p>50.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>{" "}
+            <div className="flex items-center gap-4">
+              <p>40.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
+            <div className="flex items-center gap-4">
+              <p>30.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
+            <div className="flex items-center gap-4">
+              <p>20.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
+            <div className="flex items-center gap-4">
+              <p>10.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
+            <div className="flex items-center gap-4">
+              <p>5.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
+            <div className="flex items-center gap-4">
+              <p>0.00</p>
+              <div className="w-[92%] absolute left-[8%] border-t-2 border-dashed"></div>
+            </div>
           </div>
           <div
-            className="flex flex-1 justify-around items-end text-[#525252] capitalize
+            className="flex flex-1 justify-between items-end text-[#525252] capitalize
          relative h-72"
           >
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>jan</p>
-            </div>
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-40 rounded-t-full bg-green-200"></div>
-              <p>feb</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>mar</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>apr</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>may</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>jun</p>
-            </div>
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>jul</p>
-            </div>
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-40 rounded-t-full bg-green-200"></div>
-              <p>aug</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>sep</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>oct</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>nov</p>
-            </div>{" "}
-            <div className="flex flex-col items-center gap-2 relative bottom-0">
-              <div className="w-7 h-20 rounded-t-full bg-green-200"></div>
-              <p>dec</p>
-            </div>
+            {salesData.map((data, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center gap-2 relative bottom-0"
+              >
+                <div
+                  style={{ height: `${(data.sales / 50) * 18}rem` }}
+                  className="w-7 h-20 rounded-t-full bg-green-200/50"
+                ></div>
+                <p>{data.month}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
